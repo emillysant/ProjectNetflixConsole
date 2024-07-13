47c312201d5998d685ac50c37ba947f792dac736
Leo Silva Souza <13321466+leovano@users.noreply.github.com>
2024-07-13 18:29:57 -0300

    feat: implement simple console input utilities

---
+++ src/main/java/org/example/ui/tui/ConsoleUtils.java
@@ -0,0 +1,58 @@
+package org.example.ui.tui;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class ConsoleUtils {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int getChoice(int max) {
+        return getChoice(0, max);
+    }
+
+    public static int getChoice(int min, int max) {
+        for (;;) {
+            try {
+                System.out.printf("Choose (%d-%d): ", min, max);
+                var choice = scanner.nextInt();
+                if (choice < min || choice > max)
+                    System.out.println("Invalid choice.");
+                else
+                    return choice;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid entry.");
+            }
+        }
+    }
+
+    public static String getEntry(String description) {
+        Function<String, String> defaultValidator = (it) -> it.isEmpty() ? "Cannot be empty!" : null;
+        return getEntry(description, defaultValidator);
+    }
+
+    public static String getEntry(String description, Function<String, String> validator) {
+        for (;;) {
+            System.out.printf("%s: ", description);
+            var entry = scanner.next();
+            var validation = validator.apply(entry);
+            if (validation == null)
+                return entry;
+            else
+                System.out.println(validation);
+        }
+    }
+
+    public static <R> R getEntry(String description, Function<String, R> selector, Function<R, String> validator) {
+        for (;;) {
+            System.out.printf("%s: ", description);
+            var entryID = scanner.next();
+            var entry = selector.apply(entryID);
+            var validation = validator.apply(entry);
+            if (validation == null)
+                return entry;
+            else
+                System.out.println(validation);
+        }
+    }
+}
